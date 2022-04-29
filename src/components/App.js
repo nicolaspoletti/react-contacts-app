5751554a442a9469cb9bc30b2265ee392b20c6a9
Nicolas Poletti <nicolaspoletti1@gmail.com>
2022-04-29 23:38:20 +1000

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,6 @@ function App() {
 
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
-  const contactsArray = [
-    {id: uuidv4(), name:"John Doe", phone: "(+61)0444 444 444"},
-    {id: uuidv4(), name:"Sarah Connor", phone: "(+61)0566 456 789"},
-    {id: uuidv4(), name:"Harry Potter", phone: "(+61)0555 555 555"},
-    {id: uuidv4(), name:"Albus Dumbledore", phone: "(+61)0666 666 666"},
-    {id: uuidv4(), name:"John Wick", phone: "(+61)0777 777 777"},
-    {id: uuidv4(), name:"Benjamin Buford \"Bubba\" Blue", phone: "(+61)0888 888 888"}
-  ];
 
   const addContactHandler = (contact) => {
     console.log(contact);
@@ -33,13 +25,24 @@ function App() {
   };
 
   useEffect(() => {
+    const contactsArray = [
+      {id: uuidv4(), name:"John Doe", phone: "(+61)0444 444 444"},
+      {id: uuidv4(), name:"Sarah Connor", phone: "(+61)0566 456 789"},
+      {id: uuidv4(), name:"Harry Potter", phone: "(+61)0555 555 555"},
+      {id: uuidv4(), name:"Albus Dumbledore", phone: "(+61)0666 666 666"},
+      {id: uuidv4(), name:"John Wick", phone: "(+61)0777 777 777"},
+      {id: uuidv4(), name:"Benjamin Buford \"Bubba\" Blue", phone: "(+61)0888 888 888"}
+    ];
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contactsArray));
+  }, [contacts]);
+
+  useEffect(() => {
     const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retriveContacts) setContacts(retriveContacts);
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contactsArray));
-  }, [contacts]);
+  
 
   return (
     <div className="ui container">
