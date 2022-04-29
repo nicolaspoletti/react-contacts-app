a149a7e445a42868d954397850b50cade787d366
Nicolas Poletti <nicolaspoletti1@gmail.com>
2022-04-29 23:17:29 +1000

    First Commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
   const express = require('express');
   const app = express();
   const port = process.env.PORT || 3000;
-  const publicPath = path.join(__dirname, '..', 'public');
+  const publicPath = path.join(__dirname, '..', '/public');
   
   app.use(express.static(publicPath));
 
